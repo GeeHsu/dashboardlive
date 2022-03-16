33ab33ef9023fff1268157f149c21be9fc9718b4
Gee Hsu <james81429@yahoo.com.tw>
2022-03-16 12:31:58 +0800

    後台管理：傳遞內容至 Modal 內

---
+++ javascripts/modal.js
@@ -0,0 +1,10 @@
+// 互動視窗 Modal
+const modalByDelete = document.querySelector('#deleteModal');
+modalByDelete.addEventListener('show.bs.modal', function(event) {
+  const button = event.relatedTarget;
+  const orderId = button.dataset.bsOrderId;
+  // console.log(button, orderId);
+  const modalText = modalByDelete.querySelector('#deleteText');
+
+  modalText.textContent = orderId;
+});
